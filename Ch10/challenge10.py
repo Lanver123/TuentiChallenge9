f1ddf2e7e737b32f0a52f1371d03f38c2ce72b82
Lanver123 <menoxzhan@gmail.com>
2019-05-03 20:40:36 +0200

    Ch10

---
+++ Ch10/challenge10.py
@@ -0,0 +1,31 @@
+import sys
+
+class Moon:
+  def __init__(self, orbitalPer, planetDistance, radialPos, material, nextJump):
+    self.orbitalPer = orbitalPer
+    self.planetDistance = planetDistance
+    self.radialPos = radialPos
+    self.material = material
+    self.nextJump = nextJump
+
+
+print(p1.name)
+print(p1.age)
+def distanciaLunas()
+if __name__ == "__main__":
+    inputFile = open(sys.argv[1],"r")
+    writeFile = open("out.txt","w")
+    cases = int(inputFile.readline())
+    print(cases)
+    for case in range(1,cases+1):
+        moon
+        numberMoons = int(inputFile.readline())
+        moonDistances = [float(distance) for distance in inputFile.readline().replace("\n","").split()]
+        posicionesRadiales = [float(radialPos) for radialPos in inputFile.readline().replace("\n","").split()]
+        periodosOrbitales = [float(orbitalPer) for orbitalPer in inputFile.readline().replace("\n","").split()]
+        cantidadRecolectar = [int(material) for material in inputFile.readline().replace("\n","").split()]
+        capacidadNave = int(inputFile.readline().replace("\n",""))
+        distanciaSaltoMax = float(inputFile.readline().replace("\n",""))
+        moons = []
+
+        
